52a86f77f227fa3c253d76d650ccb17b8a94c736
lightedblue <lightedblue@gmail.com>
2020-04-05 02:16:47 +0900

    ループ処理修正

---
+++ react-youtube/precache-manifest.cc766a9a8f7ddf3d6ddbc82f8f22370a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c267635d51e118d1f5ac3245b30cce93",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f590e702039a2f70d938",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ec8f2463efabc6445957",
+    "url": "./static/js/2.b0db29c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.b0db29c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f590e702039a2f70d938",
+    "url": "./static/js/main.75ce91c9.chunk.js"
+  },
+  {
+    "revision": "b726dee6a2c84958d6cd",
+    "url": "./static/js/runtime-main.5ab147fc.js"
+  }
+]);
--- react-youtube/service-worker.js
+++ react-youtube/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1850a0fb7a6ec21ee7038f568765092b.js"
+  "./precache-manifest.cc766a9a8f7ddf3d6ddbc82f8f22370a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
